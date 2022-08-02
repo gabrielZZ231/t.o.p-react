09f42fb7e03f683a16e7d9295366aa70de17c100
PRONATEC IFPE GARANHUNS <pronatec@LABS.GARANHUNS.IFPE>
2022-08-02 15:10:09 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  if (count<0) {
+    setCount(0)
+  }
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Tais fazendo o que aqui??</h1>
+      <div className="card">
+        <button className="logo" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button className="logo" onClick={() => setCount((count) => count - 1)}>
+          -
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
